5ed1864ef2f87ad9d403eb3e07052bafcb9d598b
aale-mtzg <66137018+aale-mtzg@users.noreply.github.com>
2020-10-22 01:33:42 -0600

    Vista de ubiacaiones en el sistema hecha, conexion con tabla pendiente

--- configuracionUbicaciones.php
+++ configuracionUbicaciones.php
@@ -1,3 +1,11 @@
+<?php
+
+include('conexion_db.php');
+
+$select = "SELECT * FROM ubicaciones";
+$ubicaciones = mysqli_query($conexion, $select);
+
+?>
 <!Doctype html>
 <html lang="en">
     <head>
@@ -12,13 +20,34 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <!--CSS-->
         <link href="css/inicio-style.css" rel="stylesheet" type="text/css">
+        <link href="css/configurarUbicaciones.css" rel="stylesheet" type="text/css">
         <!--icons -->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
         
 
-        <title>Inicio</title>
+        <title>Ubicaciones del sistema</title>
     </head>
+
+    <script type="text/javascript">
+		function registroNuevoUsuario(){
+			document.getElementById("formulario-crear-usuario").style.display = "block";
+			document.getElementById("tabla-de-usuarios").style.display = "none";
+        }
+
+        function nuevoTipo(){
+            document.getElementById("nuevotipoUbicacion").style.display = "block";
+        }
+
+        function validarInfomracion(){
+            
+        }
+
+
+        
+        
+	</script>
+
     <body>
         <!--Navbar-->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top">
@@ -157,10 +186,106 @@
             </nav>
             <!--Contenido principal-->
             <div id="content">
-            
+                <div class="main-container ">
+                    <!--Tabla de usuarios registrados-->
+                    <div class="row" id=tabla-de-usuarios >
+                        <H1>Ubicaciones del sistema</H1>
+                        <table class="table table-dark" >
+                            <thead>
+                                <tr>
+                                    <th scope="col">Tipo de Ubicacion</th>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Nombre del Edificio</th>
+                                    <th scope="col">Descripcion</th>
+                                    <th scope="col">Capacidad de la Ubicacion</th>
+                                    
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php while ($getresultado = $ubicaciones->fetch_assoc()) { ?>
+                                    <tr>
+                                        <!--<th scope="row"> <?php echo $getresultado['id_ubicacion'] ?> </th> -->
+                                        <td><?php echo $getresultado['tipo_ubicacion'] ?></td>
+                                        <td><?php echo $getresultado['nombre_ubicacion'] ?></td>
+                                        <td><?php echo $getresultado['nombre_edificio'] ?></td>
+                                        <td><?php echo $getresultado['descripcion_ubicacion'] ?></td>
+                                        <td><?php echo $getresultado['capacidad'] ?></td>
+                                        <!--botones--> 
+                                        
+                                        <td>
+                                            <a href="modificarUsuario.php?id_usuario=<?= $getresultado['id_ubicacion'] ?>" class="btn btn-outline-info">Modificar</a>
+                                        </td>
+                                        <td>
+                                            <a href="eliminarUsuario.php?id_usuario=<?= $getresultado['id_ubicacion'] ?>" class="btn btn-outline-danger">Eliminar</a>
+                                        </td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                        <button  type="submit" class="btn btn-primary btn-success" onclick="registroNuevoUsuario()" >Registrar nuevo usuario</button>
+                   
+                    </div>
+                    <!--Formulario de registro de nueva ubicacion (oculto hasta precionar boton)-->
+                    <div class="row oculto" id=formulario-crear-usuario >
+                        <div class="container-form-registro-usuarios">
+                            <H1>Registro de Ubicaciones:</H1>
+                            <form action="" method="POST" >
+                                <div class="form-row"> 
+                                    <div class="equipo col-12">
+                                        <div class="form-row">
+                                            <div class="form-group col-md-4">
+                                                <label for="tipoUbicacion">Ubicacion</label>
+                                                <select class="form-control" id="tipoUbicacion" name="tipoUbicacion" >
+                                                <?php // TODO ESTA LINEA DE CODIGO SOLO ES PARA TRAER LOS DATOS DE MIS TABLAS CON LA LLAVE FORANEA
+                                                    $consulta = $conexion-> query("SELECT * FROM ubicaciones");
 
+                                                    while($fila=$consulta->fetch_array()){ //recorre el arreglo
+                                                        echo "<option value ='".$fila['id_ubicacion']."'>".$fila['tipo_ubicacion']."</option>"; //muestra los datos de la tabla externa
+                                                    }
+                                                ?>
+                                                <option onclick="registroNuevoUsuario()">Otro...</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group col-md-3 oculto">
+                                                <label for="nuevotipoUbicacion">Otro</label>
+                                                <input type="text" class="form-control" id="nuevotipoUbicacion" name="nuevotipoUbicacion">    
+                                            </div>
+                                            
+                                        </div>
+                                        <div class="form-row">
+                                            <div class="col-4">
+                                                <label for="nombre">Nombre</label>
+                                                <input type="text" class="form-control" id="nombre" name="nombre">    
+                                            
+                                            </div>
+                                            <div class="col-4">
+                                                <label for="nombreEdificio">Nombre del Edificio</label>
+                                                <input type="text" class="form-control" id="nombreEdificio" name="nombreEdificio">
+                                            </div>
+                                        </div>
+                                        <div class="form-row">
+                                            <div class="form-group col-md-5">
+                                                <label for="correo">Correo:</label>
+                                                <input type="email" class="form-control" id="correo" name="correo" >      
+                                            </div>
+                                        </div>
+                                        <div class="form-row">
+                                            <div class="form-group col-md-5">
+                                                <label for="descripUbicacion">Descripcion de la Ubicacion</label>
+                                                <textarea class="form-control"  rows="3" name="descripUbicacion" type="text">
+                                                </textarea>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+        
+                                <button  type="submit" class="btn btn-primary btn-success" >Aceptar</button>
+                                <!--<button  type="btn" class="btn-success" onclick="Ocultar()"  >Ocultar</button>-->
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-
         </div>
             
             
