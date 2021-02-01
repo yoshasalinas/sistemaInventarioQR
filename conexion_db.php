35e5a274d5cd0c660369c861ff2304d115efc5d3
yoshaJani <yoshasalinas@gmail.com>
2021-01-31 23:24:42 -0700

    31/01/2021

--- conexion_db.php
+++ conexion_db.php
@@ -1,12 +1,251 @@
 <?php
 
-//conexion a db
-$conexion = new mysqli("localhost", "root", "", "inventarioactivos8");
-$resultado = mysqli_query($conexion, "SET NAMES 'utf8'"); //caracteres
+class Db{
+	
+	protected static $connection;
+    //paradigma
+	// Connect to the database
+	public function connect(){
+		if(!isset(self::$connection)){
+			$config = parse_ini_file('../config.ini');
 
-if($conexion->connect_errno){
-    echo "Error en la conexion";
-    exit; 
+			self::$connection = new mysqli($config['dbServername'], $config['dbUsername'], $config['dbPassword'], $config['dbName']);
+
+			mysqli_query(self::$connection, "SET NAMES 'utf8'");
+
+		}
+		if(self::$connection->connect_errno){
+		    echo $config['dbName'];
+			return false;
+		}
+
+		return self::$connection;
+	}
+
+	// Connect to the database
+	public function connect_services(){
+		if(!isset(self::$connection)){
+			$config = parse_ini_file('../../config.ini');
+
+			self::$connection = new mysqli($config['dbServername'], $config['dbUsername'], $config['dbPassword'], $config['dbName']);
+
+			mysqli_query(self::$connection, "SET NAMES 'utf8'");
+
+		}
+		if(self::$connection->connect_errno){
+			return false;
+		}
+
+		return self::$connection;
+	}
+
+	public function Db_query($query){
+		$connection = $this -> connect();
+
+		$result = $connection -> query($query);
+
+		return $result;
+	}
+
+	public function Db_query_save($types, $query, $postData){
+		$connection = $this -> connect();
+
+		$stmt = $connection -> prepare($query);
+
+		$stmt->bind_param($types, ...$postData);
+		$stmt->execute();
+		$stmt->close();
+	}
+
+	public function Db_query_select($types, $query, $postData){
+		$connection = $this -> connect();
+
+		$stmt = $connection -> prepare($query);
+
+		$stmt->bind_param($types, ...$postData);
+		$stmt->execute();
+
+		$result = $stmt->get_result();
+
+		if($result === false){
+			return false;
+		}
+
+        $row = $result->fetch_assoc();
+
+        foreach ($row as $rows => $value) {
+          $data[$rows] = $value;
+        }
+        $stmt->close();
+
+        return $data;
+	}
+
+	public function Db_query_select_all($types, $query, $postData){
+		$connection = $this -> connect();
+
+		$stmt = $connection -> prepare($query);
+
+		$stmt->bind_param($types, ...$postData);
+		$stmt->execute();
+
+		$result = $stmt->get_result();
+
+		if($result === false){
+			return false;
+		}
+
+		$stmt->close();
+
+		return $result;
+
+	}
+
+	public function Db_query_delete($types, $query, $postData){
+		$connection = $this -> connect();
+
+		$stmt = $connection -> prepare($query);
+
+		$stmt->bind_param($types, ...$postData);
+		$stmt->execute();
+
+		$result = $stmt->get_result();
+
+		if($result === false){
+			return false;
+		}
+
+		$stmt->close();
+
+		return true;
+	}
+
+	// ---------------------------------------- Services ------------------------------------------------
+
+	public function Db_query_services($query){
+		$connection = $this -> connect_services();
+
+		$stmt = $connection -> prepare($query);
+
+		$stmt->execute();
+
+		$result = $stmt->get_result();
+
+		if($result === false){
+			return "false"	;
+		}
+
+		$stmt->close();
+
+		return $result;
+	}
+	
+	
+	public function Db_query_save_services($types, $query, $postData){
+		$connection = $this -> connect_services();
+
+		$stmt = $connection -> prepare($query);
+
+		$stmt->bind_param($types, ...$postData);
+		$stmt->execute();
+		$stmt->close();
+	}
+
+	public function Db_query_select_all_services($types, $query, $postData){
+		$connection = $this -> connect_services();
+
+		$stmt = $connection -> prepare($query);
+
+		$stmt->bind_param($types, ...$postData);
+		$stmt->execute();
+
+		$result = $stmt->get_result();
+
+		if($result === false){
+			return "false";
+		}
+
+		$stmt->close();
+
+		return $result;
+
+	}
+
+	public function Db_query_select_all_services_2($types, $query, $postData){
+		$connection = $this -> connect_services();
+
+		$stmt = $connection -> prepare($query);
+
+		$stmt->bind_param($types, ...$postData);
+		$stmt->execute();
+
+		$result = $stmt->get_result();
+
+		$row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+		if($result === false){
+			return "false";
+		}
+
+		$stmt->close();
+
+		return $row;
+
+	}
+
+
+	public function Db_query_select_one_services($types, $query, $postData){
+		$connection = $this -> connect_services();
+
+		$stmt = $connection -> prepare($query);
+
+		$stmt->bind_param($types, ...$postData);
+
+		$stmt->execute();
+
+		$result = $stmt->get_result();
+		
+		if(mysqli_num_rows($result) > 0){
+			$row = $result->fetch_assoc();
+
+			foreach ($row as $rows => $value) {
+				$data[$rows] = $value;
+			}
+		}else{
+			$data = "False";
+		}
+        
+        $stmt->close();
+
+        return $data;
+	}
+
+	public function getLastId(){
+		$connection = $this -> connect_services();
+
+		return $connection -> insert_id;
+	}
+
+	public function Db_query_delete_services($types, $query, $postData){
+		$connection = $this -> connect_services();
+
+		$stmt = $connection -> prepare($query);
+
+		$stmt->bind_param($types, ...$postData);
+		$stmt->execute();
+
+		$result = $stmt->get_result();
+
+		if($result === true){
+			return "false";
+		}
+
+		$stmt->close();
+
+		return "true";
+	}
+
 }
+
 
 ?>
--- validarRegistroUsuario.php
+++ validarRegistroUsuario.php
@@ -6,7 +6,9 @@ include('conexion_db.php');
 $email = $_POST['correo'];
 $pass = $_POST['password'];
 
-/*
+$db = new Db();
+
+
 $error_message = "";
 if(strlen($pass)<6) {
     $error_message = "La contraseña es demasiado corta. Por favor, introduzca al menos 6 caracteres";
@@ -20,22 +22,23 @@ if(strlen($pass)<6) {
 if( ! filter_var($email, FILTER_VALIDATE_EMAIL)){
     $error_message = "Por favor, compruebe la dirección de email introducida";
 }
-*/
+if ($email ){
+
+}
+
 $consulta = "INSERT INTO usuarios (idX_rol, nombre, apellido_paterno, apellido_materno, nombre_usuario, contrasena, correo) 
- VALUES (".$_POST['rol'].",'".$_POST['nombre']."','".$_POST['aPaterno']."','".$_POST['aMaterno']."','".$_POST['nombreUsuario']."',$pass,$email)";
-/*
-if (!$consulta -> execute()){
+ VALUES (".$_POST['rol'].",'".$_POST['nombre']."','".$_POST['aPaterno']."','".$_POST['aMaterno']."','".$_POST['nombreUsuario']."','$pass','$email')";
+
+if ($conexion->query($consulta) === TRUE){
     
-    $error_message = "Error, el usuario ya existe en la base de datos";
+    header("location:usuarios.php");
    
 
 }else {
-    header("location:usuarios.php");
+    echo "error";
     
 }
-*/
-
-
+/*
 if($conexion->query($consulta) === TRUE){
 	header("location:usuarios.php");
 }else{
@@ -44,5 +47,6 @@ if($conexion->query($consulta) === TRUE){
 }
 
 mysqli_close($conexion);
+*/
 
 ?>
