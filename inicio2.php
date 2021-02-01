a4b9f755408588cde1de3d8b59bfc96446fe53df
aale-mtzg <66137018+aale-mtzg@users.noreply.github.com>
2021-01-31 18:55:10 -0700

    Navbar responsive

--- inicio2.php
+++ inicio2.php
@@ -50,99 +50,9 @@ $ubicacion = mysqli_query($conexion, $select);
 
 
     <body>
-        <nav>
-            
-            <!--Boton de menu Sidebar-->
-            <div class="menu-sidebar"><span class="fas fa-bars"></span></div>
 
-            <!--Boton de menu movil-->
-            <div class="menu-icon"><span class="fas fa-bars"></span></div>
-            <div class="logo">
-                Control de Inventario
-            </div>
-
-            <div class="nav-items">
-                <li>
-                    <a href="inicio.php"><i class="fas fa-home fa-lg"></i>Inicio</a>
-                </li>
-                <li>
-                    <a href="#movimientosSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                        <i class="fas fa-clipboard fa-lg"></i>
-                        Movimientos
-                    </a>
-                    <ul class="collapse list-unstyled" id="movimientosSubmenu">
-                        <li>
-                            <a href="cambioUbicacion.php"><i class="fas fa-thumbtack"></i>Cambio de Ubicacion</a>
-                        </li>
-                        <li>
-                            <a href="prestamo.php"><i class="fas fa-people-carry"></i>Prestamo</a>
-                        </li>
-                        <li>
-                            <a href="bajas.php"><i class="fas fa-trash-alt"></i>Bajas</a>
-                        </li>
-                    </ul> 
-                </li>
-                <li>
-                    <a href="#registroSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                        <i class="fas fa-plus fa-lg"></i>
-                        Nuevo Registro</a>
-                    <ul class="collapse list-unstyled" id="registroSubmenu">
-                        <li>
-                            <a href="registroEquipo.php"><i class="fas fa-desktop"></i>Equipo</a>
-                        </li>
-                        <li>
-                            <a href="registroMobiliario.php"><i class="fas fa-couch"></i>Mobiliario</a>
-                        </li>
-                        <li>
-                            <a href="registroRefacciones.php"><i class="fas fa-microchip"></i>Refacciones</a>
-                        </li>
-                    </ul> 
-                </li>
-                <li>
-                    <a href="inventario.php"><i class="fas fa-box-open fa-lg"></i>Inventario</a>
-                </li>
-                <li>
-                    <a href="ubicaciones.php"><i class="fas fa-map-marked-alt fa-lg"></i>Ubicaciones</a>
-                </li>
-                <li>
-                    <a href="reportes.php"><i class="fas fa-info-circle"></i>Reportes</a>
-                </li>
-                <!--Condicion para tipo de usuario: SOLO ADMINISTRADOR-->
-                <?php if($tipo_usuario == 1) { ?>
-                <li>
-                    <a href="#configuracionesSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                        <i class="fas fa-cog"></i>
-                        Configuraciones
-                    </a>
-                    <ul class="collapse list-unstyled" id="configuracionesSubmenu">
-                        <li>
-                            <a href="usuarios.php"><i class="fas fa-users"></i>Usuarios</a>
-                        </li>
-                        <li>
-                            <a href="configuracionUbicaciones.php"><i class="fas fa-map-marker-alt"></i>Ubicaciones</a>
-                        </li>
-                    </ul> 
-                </li>
-                <?php } ?>
-             
-            </div>
-
-            <img src="img/itcj-escudo-rojo.png"  class="logos-img" alt="">
-            <img src="img/logo-TNM.png"  class="logos-img" alt="">
-
-            <!--Botonwa para menu-movil-->
-            <div class="search-icon"><span class="fas fa-door-open"></span></div>
-            <div class="cancel-icon"><span class="fas fa-times"></span></div>
-
-            
-
-            <!--Boton de cerrar sesion-->
-            <div class="log-out-icon"><span class="fas fa-door-open"></span></div>
-
-        </nav>
-
-        
-            
+        <!--Nabbar-->
+        <?php include('navbar-menu-sidebar.php');  ?> 
             
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
@@ -158,32 +68,7 @@ $ubicacion = mysqli_query($conexion, $select);
             /*Archivo js para animacion en menu */ 
         </script>
 
-        <script>
-            const menuBtn = document.querySelector(".menu-icon span");
-            const searchBtn = document.querySelector(".search-icon");
-            const cancelBtn = document.querySelector(".cancel-icon");
-            const items = document.querySelector(".nav-items");
-            const form = document.querySelector("form");
-            menuBtn.onclick = ()=>{
-            items.classList.add("active");
-            menuBtn.classList.add("hide");
-            searchBtn.classList.add("hide");
-            cancelBtn.classList.add("show");
-            }
-            cancelBtn.onclick = ()=>{
-            items.classList.remove("active");
-            menuBtn.classList.remove("hide");
-            searchBtn.classList.remove("hide");
-            cancelBtn.classList.remove("show");
-            form.classList.remove("active");
-            cancelBtn.style.color = "#ff3d00";
-            }
-            searchBtn.onclick = ()=>{
-            form.classList.add("active");
-            searchBtn.classList.add("hide");
-            cancelBtn.classList.add("show");
-            }
-        </script>
+        
 
     </body>
 </html>
