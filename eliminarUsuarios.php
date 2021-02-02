69bf636b26f462ce29109f8e4f3a3bed1e1a3b9d
yoshaJani <yoshasalinas@gmail.com>
2021-02-01 23:20:10 -0700

    Agregue my object  $db

--- eliminarUsuarios.php
+++ eliminarUsuarios.php
@@ -1,3 +1,20 @@
 <?php
+include('conexion_db.php');
+
+$db = new Db();
+$id = $_GET['id'];
+$posData = [];
+$posData[0]= $id;
+
+$eliminar = "DELETE FROM usuarios WHERE id_usuario = ?";
+$result = $db-> Db_query_delete("i",$eliminar,$posData);
+
+if($result){
+    header("location:usuarios.php");
+}else{
+    
+    echo"<script> alert('No se pudo elimimar'); 
+    window.history.go(-1);</script>";
+}
 
 ?>
