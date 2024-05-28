3d667a8720a8a0aa71cbefdd9005596dac360ca2
Yosha Jani Salinas <yoshasalinas@outlook.com>
2024-05-28 02:13:24 -0600

    Descarga de imagen QR

--- downloadQR.php
+++ downloadQR.php
@@ -6,13 +6,41 @@
     $posData[0]= $id;
 
     //Buscar el archivo en la bd
-    $select = "SELECT * FROM activos WHERE numeroSerial = ?";
+    $select = "SELECT imagen_codigo_qr FROM activos WHERE numeroSerial = ?";
     $resultado = $db -> Db_query_select_all("s",$select,[$posData[0]]);
 
+
+    if($resultado){
+        if(mysqli_num_rows($resultado) == 1){
+            $fila = mysqli_fetch_assoc($resultado);             
+            $imagenBlob = $fila['imagen_codigo_qr'];
+            $data = base64_decode($imagenBlob); 
+            $imagen = imagecreatefrompng($data);
+
+           // header('Content-Type: application/octet-stream');
+            //header('Content-Disposition: attachment; filename="' . basename($imagenBlob) . '"');
+
+            header('Content-Type: image/png');
+            header('Content-Disposition: attachment; filename="' . basename($imagen) . '"');
+            imagepng($imagen);
+
+            //echo $imagen;
+            // Liberar memoria
+            imagedestroy($imagen);
+    } else {
+        echo "Imagen no encontrada en la base de datos";
+       // header("Location: registroMobiliario.php?registro=1");
+    }
+    } else {
+    echo "Error en la consulta a la base de datos";
+    }
+
+    
+/*
     if(mysqli_num_rows($resultado) == 1){
         $fila = mysqli_fetch_assoc($resultado);
         $archivo = $fila['imagen_codigo_qr'];
-        $ruta_archivo = "Files/". $archivo;
+        //$ruta_archivo = "Files/". $archivo;
 
         //Verificar que exista en el servidor
         if(file_exists($ruta_archivo)){
@@ -26,7 +54,7 @@
      } else{
         echo "El archivo no se encontro en la base de datos";
         }
-    
+    */
         //https://www.youtube.com/watch?v=-s_MRTDLhnI&ab_channel=SOFTCODEPM
 
         //tengo que convertir las imagenes blob
